7794c9402d1e6767f4d405f3ea6c488ca1bf5ac4
rgzhao <rgzhao@tencent.com>
2022-04-02 17:05:41 +0800

    feat: publish uni-app TrtcCloud SDK 1.0.0

---
+++ Api-Example/App.vue
@@ -0,0 +1,19 @@
+<script>
+	// 首先需要通过 uni.requireNativePlugin("ModuleName") 获取 module 
+	export default {
+		onLaunch() {
+			console.log('App Launch')
+		},
+		onShow() {
+			console.log('App Show')
+		},
+		onHide() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+	@import './common/trtc-nvue.css';
+</style>
---
+++ Api-Example/TrtcCloud/lib/TrtcDefines.js
@@ -0,0 +1,43 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.TRTCAudioQuality = exports.TRTCVideoStreamType = exports.TRTCRoleType = exports.TRTCAppScene = exports.TRTCParams = void 0;
+var TRTCParams = /** @class */ (function () {
+    function TRTCParams(sdkAppId, userId, roomId, userSig, strRoomId, privateMapKey, role, businessInfo, streamId, userDefineRecordId) {
+        this.sdkAppId = sdkAppId;
+        this.userId = userId;
+        this.roomId = roomId;
+        this.userSig = userSig;
+        this.strRoomId = strRoomId;
+        this.privateMapKey = privateMapKey;
+        this.role = role;
+        this.businessInfo = businessInfo;
+        this.streamId = streamId;
+        this.userDefineRecordId = userDefineRecordId;
+    }
+    return TRTCParams;
+}());
+exports.TRTCParams = TRTCParams;
+var TRTCAppScene;
+(function (TRTCAppScene) {
+    TRTCAppScene[TRTCAppScene["TRTCAppSceneVideoCall"] = 0] = "TRTCAppSceneVideoCall";
+    TRTCAppScene[TRTCAppScene["TRTCAppSceneLIVE"] = 1] = "TRTCAppSceneLIVE";
+    TRTCAppScene[TRTCAppScene["TRTCAppSceneAudioCall"] = 2] = "TRTCAppSceneAudioCall";
+    TRTCAppScene[TRTCAppScene["TRTCAppSceneVoiceChatRoom"] = 3] = "TRTCAppSceneVoiceChatRoom";
+})(TRTCAppScene = exports.TRTCAppScene || (exports.TRTCAppScene = {}));
+var TRTCRoleType;
+(function (TRTCRoleType) {
+    TRTCRoleType[TRTCRoleType["TRTCRoleAnchor"] = 20] = "TRTCRoleAnchor";
+    TRTCRoleType[TRTCRoleType["TRTCRoleAudience"] = 21] = "TRTCRoleAudience";
+})(TRTCRoleType = exports.TRTCRoleType || (exports.TRTCRoleType = {}));
+var TRTCVideoStreamType;
+(function (TRTCVideoStreamType) {
+    TRTCVideoStreamType[TRTCVideoStreamType["TRTCVideoStreamTypeBig"] = 0] = "TRTCVideoStreamTypeBig";
+    TRTCVideoStreamType[TRTCVideoStreamType["TRTCVideoStreamTypeSmall"] = 1] = "TRTCVideoStreamTypeSmall";
+    TRTCVideoStreamType[TRTCVideoStreamType["TRTCVideoStreamTypeSub"] = 2] = "TRTCVideoStreamTypeSub";
+})(TRTCVideoStreamType = exports.TRTCVideoStreamType || (exports.TRTCVideoStreamType = {}));
+var TRTCAudioQuality;
+(function (TRTCAudioQuality) {
+    TRTCAudioQuality[TRTCAudioQuality["TRTCAudioQualitySpeech"] = 1] = "TRTCAudioQualitySpeech";
+    TRTCAudioQuality[TRTCAudioQuality["TRTCAudioQualityDefault"] = 2] = "TRTCAudioQualityDefault";
+    TRTCAudioQuality[TRTCAudioQuality["TRTCAudioQualityMusic"] = 3] = "TRTCAudioQualityMusic";
+})(TRTCAudioQuality = exports.TRTCAudioQuality || (exports.TRTCAudioQuality = {}));
